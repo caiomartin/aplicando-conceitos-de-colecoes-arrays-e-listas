09ac860534aac008a4fcdf09520414d4dbf5af3d
Caio Martins <caiofernandes14@gmail.com>
2021-02-15 09:05:42 -0300

    create operations count, find e any

--- src/main/kotlin/me/caio/martins/TesteOperacoes.kt
+++ src/main/kotlin/me/caio/martins/TesteOperacoes.kt
@@ -1,7 +1,7 @@
 package me.caio.martins
 
 fun main() {
-    val salarios = doubleArrayOf(100.0, 2250.0, 4000.0)
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
     
     for(salario in salarios) {
         println(salario)
@@ -20,5 +20,18 @@ fun main() {
     salariomariosQue2500.forEach {
         println(it)
     }
+
+    println("-----------------------------------------")
+
+    println(salarios.count { it in 2000.0..5000.0})
+
+    println("-----------------------------------------")
+
+    println(salarios.find { it == 2250.0})
+    println(salarios.find { it == 500.0})
+
+    println("-----------------------------------------")
+
+    println(salarios.any { it == 1000.0})
 
 }
