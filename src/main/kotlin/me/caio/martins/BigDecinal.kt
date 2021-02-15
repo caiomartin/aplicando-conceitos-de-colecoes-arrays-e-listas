b1ddbc33488c93ecaf74b7c9cf6d5afa1c8683b7
Caio Martins <caiofernandes14@gmail.com>
2021-02-15 10:34:58 -0300

    create  Extensions Functions  class

---
+++ src/main/kotlin/me/caio/martins/BigDecinal.kt
@@ -0,0 +1,11 @@
+package me.caio.martins
+
+import java.math.BigDecimal
+
+fun Array<BigDecimal>.somatoria() = this.reduce {
+    acc, valor -> acc +valor
+}
+
+fun Array<BigDecimal>.media()=
+    if(this.isEmpty()) BigDecimal.ZERO
+    else this.somatoria()/this.size.toBigDecimal()
---
+++ src/main/kotlin/me/caio/martins/TesteFubcoesEstendidas.kt
@@ -0,0 +1,15 @@
+package me.caio.martins
+
+fun main() {
+    val salarios = arrayOf(
+        "2000".toBigDecimal(),
+        "1500".toBigDecimal(),
+        "4000".toBigDecimal()
+    )
+
+    println(salarios.somatoria())
+
+    println("---------------------------")
+
+    println(salarios.media())
+}
