75e03f5dc465b8d75ca1cc0582b1adf5d2013c45
Caio Martins <caiofernandes14@gmail.com>
2021-02-14 22:39:47 -0300

    create double array of class

--- src/main/kotlin/me/caio/martins/TesteDoubleArray.kt
+++ src/main/kotlin/me/caio/martins/TesteDoubleArray.kt
@@ -1,2 +1,25 @@
 package me.caio.martins
 
+fun main() {
+    val salarios = DoubleArray(3)
+    salarios[0] = 100.0
+    salarios[1] = 10000.0
+    salarios[2] = 500.0
+    
+    salarios.forEach { println(it)}
+
+    println("-------------------------------------")
+    
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario * 1.1
+    }
+    salarios.forEach { println(it) }
+
+    println("-------------------------------------")
+    
+    val salarios2 =  doubleArrayOf(1500.0,  1250.0, 5000.0)
+    
+    salarios2.sort()
+    salarios2.forEach { println(it)}
+    
+}
--- src/main/kotlin/me/caio/martins/testeArrayString.kt
+++ src/main/kotlin/me/caio/martins/testeArrayString.kt
@@ -1,2 +1,23 @@
 package me.caio.martins
 
+fun main() {
+    val nomes = Array(3){""}
+
+    nomes[0] = "Maria"
+    nomes[1] = "João"
+    nomes[2] = "José"
+
+    for (nome in nomes) {
+        println(nome)
+    }
+    println("-------------------------------------")
+    nomes.sort()
+    nomes.forEach {
+        println(it)
+    }
+    println("-------------------------------------")
+    val nomes2 = arrayOf("Maria", "Lucas", "Pedro")
+    nomes2.sort()
+    nomes2.forEach { println(it)}
+
+}
