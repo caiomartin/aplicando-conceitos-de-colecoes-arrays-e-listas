7bc0587834d7646f4df03754366a54a83a27e783
Caio Martins <caiofernandes14@gmail.com>
2021-02-15 09:45:26 -0300

    create seOf class = union, subtract and intersect

---
+++ src/main/kotlin/me/caio/martins/TesteSet.kt
@@ -0,0 +1,24 @@
+package me.caio.martins
+
+fun main() {
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1000.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val funcionario1 = setOf(joao, pedro)
+    val funcionario2 = setOf(maria)
+    
+    val resultUnion = funcionario1.union(funcionario2)
+    resultUnion.forEach{println(it)}
+
+    println("----------------------------------")
+    val funcionario3 = setOf(joao, pedro, maria)
+    val resultSubtract = funcionario3.subtract(funcionario2)
+    resultSubtract.forEach{println(it)}
+
+    println("----------------------------------")
+    val resultIntersect = funcionario3.intersect(funcionario2)
+    resultIntersect.forEach{println(it)}
+
+
+}
